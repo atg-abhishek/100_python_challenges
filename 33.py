806d5d8b2853e000c903ae5b28dcad2de316e724
atg.abhishek <atg.abhishek@gmail.com>
2017-01-15 08:23:28 -0500

    solved challenge 33 , 7.5 in the link

---
+++ 33.py
@@ -0,0 +1,14 @@
+'''
+Question:
+Define a class named Circle which can be constructed by a radius. The Circle class has a method which can compute the area. 
+
+'''
+import math
+class Circle():
+    def __init__(self, radius):
+        self.radius = radius
+    def area(self):
+        return math.pi*self.radius*self.radius
+
+a = Circle(5)
+print(a.area())
