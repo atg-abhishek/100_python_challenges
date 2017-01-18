69f65441a3f3d69d8ddc68a574e1f70109fcc17a
atg.abhishek <atg.abhishek@gmail.com>
2017-01-17 20:31:11 -0500

    solved challenge 35, 7.2 in the link

---
+++ 35.py
@@ -0,0 +1,23 @@
+'''
+7.2
+
+Define a class named Shape and its subclass Square. The Square class has an init function which takes a length as argument. Both classes have a area function which can print the area of the shape where Shape's area is 0 by default.
+
+'''
+
+class Shape():
+    def __init__(self):
+        pass
+        
+    def area(self):
+        return 0
+
+class Square(Shape):
+    def __init__(self, l):
+        self.length = l
+    
+    def area(self):
+        return self.length*self.length
+
+a = Square(5)
+print(a.area())
